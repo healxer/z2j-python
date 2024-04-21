f7b6b09cd91d0c90b86cd440135f2a5548929a2b
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-04-21 22:01:05 +0200

    Finish exercice 4.1 What is a string

---
+++ f-101/chapter4-strings-exercices/4.1_what_is_a_string.py
@@ -0,0 +1,19 @@
+# 1. Print a string that uses double quotation marks inside the string.
+
+print('This is "Zero To Junior".')
+
+# 2. Print a string that uses an apostrophe inside the string.
+
+print("I'm a future developer!")
+
+# 3. Print a string that spans multiple lines, with whitespace preserved.
+
+print("""Triple-quoted strings have a
+      special purpose in Python.
+        They are used to document code.""")
+
+# 4. Print a string that is coded on multiple lines but displays on a single line.
+
+print("When used to document code,\
+triple-quoted strings are called \
+docstrings.")
