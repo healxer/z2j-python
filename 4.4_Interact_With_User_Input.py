69e16af09cc36582afd51bd71c50b9a589b85e3a
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-04-21 22:58:05 +0200

    Finish 4.4 Interact with user input exercise

---
+++ 4.4_Interact_With_User_Input.py
@@ -0,0 +1,13 @@
+# 1. Write a script that takes input from the user and displays that input back.
+
+prompt = input("Welcome to Zero To Junior, please type your name: ")
+print(prompt)
+
+# 2. Write a script that takes input from the user and displays the input in lowercase.
+
+user_prompt = prompt.lower()
+print(user_prompt)
+
+# 3. Write a script that takes input from the user and displays the number of characters inputted.
+
+print(len(prompt))
