6d5809eb117e79aa4e5246eaff005b3efffdc8c4
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-05-02 21:08:09 +0200

    Finish 5.6 Print numbers in style exercise

---
+++ f-101/chapter5-numbers-and-math/5.6_print_numbers_in_style.py
@@ -0,0 +1,14 @@
+# 1. Print the result of the calculation 3 ** .125 as a fixed-point number with three decimal places.
+
+n = 3**0.125
+print(f"{n:.3f}")
+
+# 2. Print the number 150000 as currency, with the thousands grouped with commas. Currency should be displayed with two decimal places.
+
+num = 150000
+print(f"{num:,.2f}")
+
+# 3. Print the result of 2 / 10 as a percentage with no decimal places. The output should look like 20%.
+
+percentage = 2 / 10
+print(f"{percentage:.0%}")
