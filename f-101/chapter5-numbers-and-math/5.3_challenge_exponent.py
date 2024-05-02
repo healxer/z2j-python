c37ff6f38f2ce35e8d8c2645e79e82d01e8e25a2
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-05-02 21:04:55 +0200

    Finish 5.3 Challenge Perform calculations on user input

---
+++ f-101/chapter5-numbers-and-math/5.3_challenge_exponent.py
@@ -0,0 +1,9 @@
+# Write a script called exponent.py that receives two numbers from the user and displays the first number raised to the power of the second number.
+
+base = float(input("Enter a base: "))
+exponent = int(input("Enter an exponent: "))
+exponentiation = base**exponent
+
+result = f"{base} to the power of {exponent} = {exponentiation}"
+
+print(result)
