df2af47c4626f2b7d1b70925ca131175294f504b
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-05-02 20:52:19 +0200

    Finish 4.9 Challenge

---
+++ f-101/chapter4-strings-exercises/4.9_challenge_translate.py
@@ -0,0 +1,4 @@
+text = input("Enter some text: ")
+leetspeak = text.replace("a", "4").replace("b", "80").replace("e", "3").replace("l", "1").replace("o", "0").replace("s", "5").replace("t", "7")
+
+print(leetspeak)
