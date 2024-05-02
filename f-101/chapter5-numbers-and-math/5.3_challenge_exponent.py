8e8ae772906ca7407ffea627ec82e7e27cef7ea2
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-05-02 21:11:28 +0200

    Change from int to float

--- f-101/chapter5-numbers-and-math/5.3_challenge_exponent.py
+++ f-101/chapter5-numbers-and-math/5.3_challenge_exponent.py
@@ -1,7 +1,7 @@
 # Write a script called exponent.py that receives two numbers from the user and displays the first number raised to the power of the second number.
 
 base = float(input("Enter a base: "))
-exponent = int(input("Enter an exponent: "))
+exponent = float(input("Enter an exponent: "))
 exponentiation = base**exponent
 
 result = f"{base} to the power of {exponent} = {exponentiation}"
